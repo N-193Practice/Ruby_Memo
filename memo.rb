4ed4bec3fbc656add6dcff8d86e1bb7ed7a81de2
N-193Practice <keep_me_nk0914@icloud.com>
2023-10-15 20:09:06 +0900

    Update memo.rb
    修正

--- memo.rb
+++ memo.rb
@@ -7,6 +7,10 @@ if user_choice == 1
   puts "新しいメモを入力してください："
   new_memo = gets.chomp
 
+  # 新規作成時
+  puts "新しいファイル名を入力してください"
+  file_name = gets.chomp
+
   # 新規ファイルを作成し、新しいメモを追加
   CSV.open('memos.csv', 'w') do |csv|
     csv << [new_memo]
@@ -26,6 +30,10 @@ elsif user_choice == 2
   puts "新しい内容を入力してください："
   new_content = gets.chomp
 
+  # 編集時
+  puts "編集するメモのファイル名を入力してください"
+  file_name = gets.chomp
+
   # メモを編集してファイルに書き込み
   memos = CSV.read('memos.csv')
   memos[memo_number - 1] = [new_content]
@@ -38,4 +46,17 @@ elsif user_choice == 2
   puts "メモが編集されました。"
 else
   puts "無効な選択です。"
+
+# 新規作成時及び編集時の入力部分Ctrl+D検出
+  puts "新しいメモを入力してください。Ctrl+Dで確定:"
+  new_memo = ""
+  while (line = $stdin.gets)
+    new_memo += line  
+end
+
+  puts "新しい内容を入力してください。Ctrl+Dで確定:"
+  new_content = ""
+  while (line = $stdin.gets)
+    new_content += line
+end
 end
